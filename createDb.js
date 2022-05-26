a60a59eb7f916fb3692c5507a3f4f7c94961e217
viktor-gif <klim.victor.ua@gmail.com>
2022-05-26 13:09:26 +0300

    mongoose connection, async, async.series

--- createDb.js
+++ createDb.js
@@ -1,17 +1,78 @@
 const User = require('./models/user')
+const mongoose = require('./libs/mongoose')
+const async = require('async')
 
-
-console.log(typeof User)
-
-const user = new User({
-    username: "Tester6",
-    password: "secret"
+async.series([
+    open,
+    dropDatabase,
+    createUsers,
+    close
+], function (err, results) {
+    console.log(results);
 })
 
-user.save((err, user, affected) => {
-    if (err) throw err
+function open(callback) {
+    mongoose.connection.on('open', callback)
+}
+function dropDatabase(callback) {
+    const db = mongoose.connection.db;
+    db.dropDatabase(callback)
+}
+function createUsers(callback) {
+    async.parallel([
+        function (callback) {
+            const vasya = new User({ username: 'Вася', password: 'supervasya' })
+            vasya.save((err) => {
+                callback(err, vasya)
+            })
+        },
+        function (callback) {
+            const petya = new User({ username: 'Петя', password: '12345' })
+            petya.save((err) => {
+                callback(err, petya)
+            })
+        },
+        function (callback) {
+            const admin = new User({ username: 'admin', password: 'thenthis' })
+            admin.save((err) => {
+                callback(err, admin)
+            })
+        }
+    ], callback)
+}
+function close(callback) {
+    mongoose.disconnect(callback)
+}
 
-    User.findOne({ username: "Tester" }, function (err, tester) {
-        console.log(tester);
-    })
-})
+// mongoose.connection.on('open', () => {
+//     const db = mongoose.connection.db;
+
+//     db.dropDatabase((err) => {
+//         if (err) throw err
+//         console.log('OK');
+
+//         async.parallel([
+//             function (callback) {
+//                 const vasya = new User({ username: 'Вася', password: 'supervasya' })
+//                 vasya.save((err) => {
+//                     callback(err, vasya)
+//                 })
+//             },
+//             function (callback) {
+//                 const petya = new User({ username: 'Петя', password: '12345' })
+//                 petya.save((err) => {
+//                     callback(err, petya)
+//                 })
+//             },
+//             function (callback) {
+//                 const admin = new User({ username: 'admin', password: 'thenthis' })
+//                 admin.save((err) => {
+//                     callback(err, admin)
+//                 })
+//             }
+//         ], function (err, results) {
+//             console.log(results);
+//             mongoose.disconnect()
+//         })    
+//     })
+// })
